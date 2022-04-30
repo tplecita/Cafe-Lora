d5e91f981a64228ed9ecbf5d585cf59a22037d67
Tereza Plecitá <plecita.terez@gmail.com>
2022-04-30 11:29:18 +0200

    Zprovoznění navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
-console.log('funguju!');
+const navElm = document.querySelector('#nav-btn');
+const nav = document.querySelector('nav');
+
+const hideNavigation = () => {
+  nav.classList.toggle('nav-closed');
+};
+
+navElm.addEventListener('click', () => {
+  hideNavigation();
+});
+
+const navContentArray = document.querySelectorAll('a');
+
+navContentArray.forEach((item) => {
+  item.addEventListener('click', () => {
+    hideNavigation();
+  });
+});
