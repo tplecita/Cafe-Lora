2da5240312b36d30efd0e1812925ef4e02cea109
Tereza Plecitá <plecita.terez@gmail.com>
2022-04-30 12:12:27 +0200

    Připravena funkce pro objednávání

--- src/index.js
+++ src/index.js
@@ -14,7 +14,22 @@ navElm.addEventListener('click', () => {
 const navContentArray = document.querySelectorAll('a');
 
 navContentArray.forEach((item) => {
-  item.addEventListener('click', () => {
-    hideNavigation();
-  });
+  item.addEventListener('click', hideNavigation);
+});
+
+const objElm = document.querySelector('.order-btn');
+let ordered = false;
+
+objElm.addEventListener('click', () => {
+  if (ordered === true) {
+    objElm.textContent = 'Zrušit';
+    const objednavka = document.querySelector('.drink__cup');
+    objednavka.classList.add('drink__cup--selected');
+    ordered = false;
+  } else {
+    objElm.textContent = 'Objednat';
+    const objednavka = document.querySelector('.drink__cup');
+    objednavka.classList.remove('drink__cup--selected');
+    ordered = true;
+  }
 });
